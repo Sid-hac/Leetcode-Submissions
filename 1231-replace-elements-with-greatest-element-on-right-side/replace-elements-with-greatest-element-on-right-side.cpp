09f1751b882b73834a9beaf8c460cb9353a60f57
Sid <131300504+Sid-hac@users.noreply.github.com>
2025-06-05 13:01:35 +0530

    Time: 8 ms (24.49%) | Memory: 72.8 MB (19.33%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
@@ -2,29 +2,44 @@ class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
 
-        vector<int>ans;
+        // vector<int>ans;
 
-        if(arr.size() == 1){
-            ans.push_back(-1);
-            return ans;
-        }
+        // if(arr.size() == 1){
+        //     ans.push_back(-1);
+        //     return ans;
+        // }
 
-        for(int i = 0; i < arr.size(); i++){
+        // for(int i = 0; i < arr.size(); i++){
 
-                int maxi = 0;
-                if(i == arr.size()-1){
-                    ans.push_back(-1);
-                    return ans;
-                }
-            for(int j = i+1; j < arr.size(); j++){
+        //         int maxi = 0;
+        //         if(i == arr.size()-1){
+        //             ans.push_back(-1);
+        //             return ans;
+        //         }
+        //     for(int j = i+1; j < arr.size(); j++){
 
-                maxi = max(maxi , arr[j]);
+        //         maxi = max(maxi , arr[j]);
+        //     }
+        //     ans.push_back(maxi);
+        // }
+
+        // return ans;
+                stack<int> s;
+        s.push(-1);
+        int n = arr.size();
+        vector<int> ans(n);
+
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] = s.top();
+            while(s.top() != -1 && s.top() < arr[i]) {
+                s.pop();
             }
-            ans.push_back(maxi);
+            if (arr[i] > s.top())
+                s.push(arr[i]);
         }
 
         return ans;
 
-        
+    
     }
 };
