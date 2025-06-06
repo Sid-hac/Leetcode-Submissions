c519f5bd06f04d61c357d644cd61bdd1a0425d06
Sid <131300504+Sid-hac@users.noreply.github.com>
2025-06-06 22:39:29 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (34.60%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
@@ -1,39 +1,50 @@
 class Solution {
 public:
     int appendCharacters(string s, string t) {
-        stack<char>stack;
-        int count = 0;
+        // stack<char>stack;
+        // int count = 0;
 
-        for(int i = s.size() - 1; i >= 0; i--){
-              stack.push(s[i]);
-        }
+        // for(int i = s.size() - 1; i >= 0; i--){
+        //       stack.push(s[i]);
+        // }
 
-        for(int i = 0; i < t.size(); i++){
+        // for(int i = 0; i < t.size(); i++){
 
-            if( !stack.empty() && t[i] == stack.top()){
-                stack.pop();
-                count++;
-            }else{
+        //     if( !stack.empty() && t[i] == stack.top()){
+        //         stack.pop();
+        //         count++;
+        //     }else{
                  
-                while(!stack.empty() && t[i] != stack.top()){
-                    stack.pop();
-                }
+        //         while(!stack.empty() && t[i] != stack.top()){
+        //             stack.pop();
+        //         }
 
-                 if (!stack.empty() && t[i] == stack.top()) {
-                      stack.pop();
-                      count++;
-                 } else {
-                      // Stack is empty and no match found
-                      break;
-                 }
-            }
+        //          if (!stack.empty() && t[i] == stack.top()) {
+        //               stack.pop();
+        //               count++;
+        //          } else {
+        //               // Stack is empty and no match found
+        //               break;
+        //          }
+        //     }
+        // }
+
+        // if(stack.empty() && count != t.size()){
+        //             return t.size() - count;
+        // }
+
+        // return 0;
+
+        int i = 0;
+        int j = 0;
+
+        while(i < s.size() && j < t.size()){
+              
+              if(s[i] == t[j]) j++;
+              i++;
         }
 
-        if(stack.empty() && count != t.size()){
-                    return t.size() - count;
-        }
-
-        return 0;
+        return t.size() - j;
 
         
     }
